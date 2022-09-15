fc5197b13436dd9770cdee1aa95e0daf3d0becae
Mac L <macattackairman@gmail.com>
2022-09-14 21:20:19 -0400

    added scripts

---
+++ attack.py
@@ -0,0 +1,48 @@
+import requests
+import time
+import sys
+from colorama import Fore, Back, Style
+
+import arb_lib
+
+
+acaii = '''
+     _________
+    / ======= \
+   / __________\
+  | ___________ |
+  | |crypto   | |
+  | |arbitrage| |
+  | |tool     | |
+  | |_________| |__________________________
+  \=____________/   by @mac-lawson         )
+  / """"""""""" \                          /
+ / ::::::::::::: \  github.com/mac-lawson -'
+(_________________)
+
+'''
+
+
+
+############
+
+def run():
+
+# Place functions to run here
+
+
+
+
+#############
+
+
+
+arb_lib.coolPrint(acaii)
+time.sleep(2)
+
+while True:
+	
+	mainMenu()
+
+
+
